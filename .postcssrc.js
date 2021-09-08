da9d5a9ba8dfe25236f20816d7bc42739de8e20c
JongYeobPark <pjyub1297@gmail.com>
2021-09-08 15:52:17 +0900

    Create Project

---
+++ .postcssrc.js
@@ -0,0 +1,15 @@
+//ESM     (브라우저 환경 사용 방식)
+//CommonJS(nodejs 환경 사용 방식)
+
+//import = require()
+//export = module.exports
+
+//import autoprefixer from 'autoprefixer'
+//const autoprefixer = require('autoprefixer')
+
+module.exports = {
+  plugins: [
+    //autoprefixer
+    require('autoprefixer')
+  ]
+}
